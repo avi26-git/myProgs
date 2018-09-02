56c7ec835f8dbb40205d2d765f82891ec393c13e
Abhishek Verma <aviverma1101@gmail.com>
2018-09-02 19:04:29 +0530

    Print staircase pattern

---
+++ Staircase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllDemos
+{
+    class Staircase
+    {
+        // Complete the staircase function below.
+        static void staircase(int arr)
+        {
+            for (int i = 0; i < arr; i++)
+            {
+                string print = ReturnStars().Take(i + 1).Aggregate((a, b) => a + b);
+                List<string> test = ReturnStars().Take(i + 1).ToList();
+                Console.Write(print.PadLeft(arr));
+                Console.WriteLine();
+            }
+
+        }
+        static IEnumerable<string> ReturnStars()
+        {
+            while (1 == 1)
+            {
+                yield return "#";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
